a25c1dfa14ebbb637776077c9b2bb75bbbf9e835
Salshafeay <78335103+Salshafeay@users.noreply.github.com>
2021-07-29 13:15:02 -0500

    adding example1

--- example1.py
+++ example1.py
@@ -1,1 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+x=[0,1,2,3,4]
+y=[0,2,4,6,8]
+
+plt.plot(x, y)
+plt.title('My First Gragh')
+
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+plt.show()
